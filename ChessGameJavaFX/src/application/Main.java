db877829e6913cb9903777713c7574dc323d9b29
Mikewro <Mikewro@LAPTOP-KOHOT39K>
2021-01-01 22:13:58 +0100

    First attempt to draw a chessboard

--- ChessGameJavaFX/src/application/Main.java
+++ ChessGameJavaFX/src/application/Main.java
@@ -1,26 +1,63 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 
 public class Main extends Application {
-	@Override
+
+	GridPane root = new GridPane();
+	final int size = 10;
+
 	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
+
+		for (int row = 0; row < size - 2; row++) {
+			for (int col = 0; col < size - 2; col++) {
+				Rectangle square = new Rectangle();
+				Color color;
+//           parzystoœæ sum kolorów decyduje o kolorze, parzysty jest bia³y
+				if ((row + col) % 2 == 0)
+					color = Color.WHITE;
+				else
+					color = Color.BLACK;
+				square.setFill(color);
+
+				root.add(square, col, row);
+				// Setting size for the pane
+				root.setMinSize(400, 400);
+
+				// Setting the padding
+				root.setPadding(new Insets(20, 20, 20, 20));
+
+//				// Setting the vertical and horizontal gaps between the columns
+//				root.setVgap(15);
+//				root.setHgap(5);
+
+				// Setting the Grid alignment
+				root.setAlignment(Pos.CENTER);
+
+				square.widthProperty().bind(root.widthProperty().divide(size));
+				square.heightProperty().bind(root.heightProperty().divide(size));
+			}
 		}
+
+		root.add(new Button("Reset/New"), 3, 11);
+
+//		root.getChildren().addAll(new Rectangle(800,800,Color.ANTIQUEWHITE), new );
+
+		primaryStage.setScene(new Scene(root, 800, 800));
+		primaryStage.show();
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
+
 }
